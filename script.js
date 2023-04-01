1928d9fe6788adff174ffee51953525c3c82f9a4
Ruby pal <rubyp20001@gmail.com>
2023-04-01 10:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let text = document.getElementById("evaluatedText");
+let count = document.getElementById("wordCount");
+
+
+text.addEventListener("input" , evaluate)
+
+function evaluate(){
+    let word = (text.value).split(" ").length;
+    count.innerText = word;
+
+}
